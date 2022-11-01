e477da65be9f1c0700c8ce75011418bd185cb162
MustafaBerkanVay <berkanvay@gmail.com>
2022-11-01 19:16:55 +0300

    Editing files.

---
+++ E-Commerce/REST Client/Models/ProductResponse.swift
@@ -0,0 +1,8 @@
+//
+//  ProductResponse.swift
+//  E-Commerce
+//
+//  Created by Mustafa Berkan Vay on 1.11.2022.
+//
+
+import Foundation
---
+++ E-Commerce/REST Client/RESTClient.swift
@@ -0,0 +1,8 @@
+//
+//  RESTClient.swift
+//  E-Commerce
+//
+//  Created by Mustafa Berkan Vay on 1.11.2022.
+//
+
+import Foundation
